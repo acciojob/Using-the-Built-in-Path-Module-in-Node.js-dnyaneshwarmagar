07d00c5e88090e96b0b68f1dc601629537d25e90
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 10:30:40 +0530

    done

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
-// TODO: Use path.join to join two file path segments
+// Ensure the script is called with the correct number of arguments
+if (process.argv.length !== 4) {
+  console.error('Usage: node app.js <pathSegment1> <pathSegment2>');
+  process.exit(1);
+}
 
-// TODO: Print the resulting path to the console
+// Get the file path segments from command-line arguments
+const pathSegment1 = process.argv[2];
+const pathSegment2 = process.argv[3];
+
+// Join the path segments
+const joinedPath = path.join(pathSegment1, pathSegment2);
+
+// Print the joined path to the console
+console.log(joinedPath);
